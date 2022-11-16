b15eb9362fc0ee3926b34e3949787d813745d034
smoy <smoy27@uic.edu>
2022-11-16 20:53:04 +0000

    add newfile and some fixes

---
+++ .ccls-cache/@home@runner@Lab-13-Git/main.c
@@ -0,0 +1,30 @@
+// Collaborative code file, Lab 13
+// 
+// One person in your partner group should clone this repository.
+// Create a function that outputs your name, along with either one thing 
+//    that you learned about git or GitHub or something that gave you trouble
+//    that you would like others to learn from. The function name should be
+//    your NetID combined with your partner's NetID.
+// Feel free to add other things to your function as well, though be mindful
+// of the length of it, given how many students will be collaborating on this code!
+// 
+// Read through the code that is already here before adding your own.
+// Add this code to the repository using git commands.
+// 
+// An example is shown below.
+// 
+// UIC CS 211, Fall 2022
+#include <stdio.h>
+
+void smoy27(){
+  printf("Steven Moy\n");
+  printf("I learned that GitHub is great for collaboration between multiple people and also that it stores our own history for easier access.");
+}
+
+int main(){
+    printf("This code has been created by the students of CS 211, Fall 2022!\n\n");
+    
+    smoy27();
+    
+    return 0;
+}
